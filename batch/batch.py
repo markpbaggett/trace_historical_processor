c04fb82a9c1488a7abe817ee04ee3f62e8e7ab27
Mark Baggett <mbagget1@utk.edu>
2019-02-06 08:16:15 -0500

    Make a tiny number of changes.

--- batch/batch.py
+++ batch/batch.py
@@ -9,6 +9,7 @@ class Dissertations:
 
     @staticmethod
     def read_etds(path):
+        our_new_files = []
         for filename in os.walk(path):
             our_new_files = filename[2]
         return our_new_files
@@ -18,25 +19,40 @@ class MetadataReader:
     def __init__(self, filename, etds):
         self.file = filename
         self.etds = etds
-        self.rows = self.read_contents()
+        self.bad_metadata = []
+        self.urls = self.read_contents()
 
     def read_contents(self):
-        with open(self.file, 'r') as our_csv_file:
+        with open(self.file, "r") as our_csv_file:
             reader = csv.reader(our_csv_file, delimiter="|")
             urls_for_spreadsheet = []
             for row in reader:
                 try:
-                    starting_text = f"{row[6]}{row[4]}"
+                    starting_text = f"{row[6].rstrip()}{row[4].rstrip()}"
+                    my_url = ""
                     for dissertation in self.etds:
                         if dissertation.startswith(starting_text):
-                            del row[1]
-                            row.insert(1, f"http://dlshare.lib.utk.edu/historical/{dissertation}")
-                    urls_for_spreadsheet.append(row)
+                            my_url = (
+                                f"http://dlshare.lib.utk.edu/historical/{dissertation}"
+                            )
+                    if my_url == "":
+                        self.bad_metadata.append(f"{row[6]}{row[4]}")
+                    urls_for_spreadsheet.append(my_url)
                 except IndexError:
-                    print("error")
+                    urls_for_spreadsheet.append("")
             return urls_for_spreadsheet
+
+    def write_urls_to_file(self):
+        with open("current_urls.txt", "w") as output_text:
+            for url in self.urls:
+                output_text.write(f"{url}\n")
 
 
 if __name__ == "__main__":
-    dissertations = Dissertations("/home/mark/PycharmProjects/historical_etds/data/2003disserations").contents
-    MetadataReader("/home/mark/PycharmProjects/historical_etds/mark_test_2003_dissertations.csv", dissertations).rows
+    dissertations = Dissertations(
+        "/home/mark/PycharmProjects/historical_etds/data/2003disserations"
+    ).contents
+    MetadataReader(
+        "/home/mark/PycharmProjects/historical_etds/mark_test_2003_dissertations.csv",
+        dissertations,
+    ).write_urls_to_file()
